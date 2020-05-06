cf92d10b6cbb230bff36b249fc06807ef783ba27
Kasturi Joshi <48174045+kasturijoshi06@users.noreply.github.com>
2020-05-05 21:34:44 -0500

    Add files via upload

---
+++ data/bar_race.py
@@ -0,0 +1,17 @@
+def bar_race(monthly):
+    import pandas as pd
+    race_df = monthly
+    temp1  = race_df
+    temp1['year'] = pd.DatetimeIndex(temp1['Month_Beginning']).year
+    temp1 = temp1.drop(["Month_Beginning"],axis =1)
+    temp1 = temp1.groupby(["routename","year"]).mean().unstack()
+    #temp1 =temp1.nlargest(50,"MonthTotal")
+    # routes = list(temp1["routename"])
+    # routes
+    temp1.columns = temp1.columns.get_level_values(1)
+    temp1 = temp1.reset_index()
+    display(temp1)
+    temp1.to_csv('bar_race.csv', index=False)
+    race_df = race_df.loc[race_df["routename"].isin(routes)]
+    race_df.to_csv('bar_race.csv', index=False)
+    return race_df
